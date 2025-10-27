5b7b13e469022a8d9c418458a701cbb5b46251b1
kihochalong <41330247@nfu.edu.tw>
2025-10-27 13:49:26 +0800

    Update cbuiltindig.cpp

--- cbuiltindig.cpp
+++ cbuiltindig.cpp
@@ -4,7 +4,7 @@
 CBuiltinDig::CBuiltinDig(QWidget *parent)
     : QDialog(parent)
 {
-    QGrindLayout *gridLayout = new QGridLayout;
+    QGridLayout *gridLayout = new QGridLayout;
     displayTextEdit = new QTextEdit(QStringLiteral("幹你娘"));
     colorPushBtn    = new QPushButton(QStringLiteral("顏色對話"));
     errorPushBtn    = new QPushButton(QStringLiteral("錯誤訊息"));
@@ -14,6 +14,20 @@ CBuiltinDig::CBuiltinDig(QWidget *parent)
     pagePushBtn     = new QPushButton(QStringLiteral("頁面設定對話"));
     progressPushBtn = new QPushButton(QStringLiteral("進度對話"));
     printPushBtn    = new QPushButton(QStringLiteral("印出對話"));
+
+    gridLayout->addWidget(colorPushBtn,0,0,1,1);
+    gridLayout->addWidget(errorPushBtn,0,1,1,1);
+    gridLayout->addWidget(filePushBtn,0,2,1,1);
+    gridLayout->addWidget(fontPushBtn,1,0,1,1);
+    gridLayout->addWidget(inputPushBtn,1,1,1,1);
+    gridLayout->addWidget(pagePushBtn,1,2,1,1);
+    gridLayout->addWidget(progressPushBtn,2,0,1,1);
+    gridLayout->addWidget(printPushBtn,2,1,1,1);
+    gridLayout->addWidget(displayTextEdit,3,0,3,3);
+
+    setLayout(gridLayout);
+    setWindowTitle(QStringLiteral("內建對話盒展示"));
+    resize(400,300);
 }
 
 CBuiltinDig::~CBuiltinDig() {}
