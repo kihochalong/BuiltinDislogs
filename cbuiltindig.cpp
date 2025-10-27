ee485762a22912ce2114817d40c4002f59f23a77
kihochalong <41330247@nfu.edu.tw>
2025-10-27 15:30:34 +0800

    更新
    顏色，錯誤，進度

--- cbuiltindig.cpp
+++ cbuiltindig.cpp
@@ -1,5 +1,8 @@
 #include "cbuiltindig.h"
 #include <QGridLayout>
+#include <QDebug>
+#include <QPalette>
+#include <QtWidgets>
 
 CBuiltinDig::CBuiltinDig(QWidget *parent)
     : QDialog(parent)
@@ -28,6 +31,68 @@ CBuiltinDig::CBuiltinDig(QWidget *parent)
     setLayout(gridLayout);
     setWindowTitle(QStringLiteral("內建對話盒展示"));
     resize(400,300);
+    connect(colorPushBtn,SIGNAL(clicked()),this,SLOT(doPushBtn()));
+    connect(errorPushBtn,SIGNAL(clicked()),this,SLOT(doPushBtn()));
+    connect(filePushBtn,SIGNAL(clicked()),this,SLOT(doPushBtn()));
+    connect(fontPushBtn,SIGNAL(clicked()),this,SLOT(doPushBtn()));
+    connect(inputPushBtn,SIGNAL(clicked()),this,SLOT(doPushBtn()));
+    connect(pagePushBtn,SIGNAL(clicked()),this,SLOT(doPushBtn()));
+    connect(progressPushBtn,SIGNAL(clicked()),this,SLOT(doPushBtn()));
+    connect(printPushBtn,SIGNAL(clicked()),this,SLOT(doPushBtn()));
+    connect(displayTextEdit,SIGNAL(clicked()),this,SLOT(doPushBtn()));
+
 }
 
 CBuiltinDig::~CBuiltinDig() {}
+void CBuiltinDig::doPushBtn()
+{
+    QPushButton* btn = qobject_cast<QPushButton*>(sender());
+    if(btn == colorPushBtn)
+    {
+        QPalette palette = displayTextEdit->palette();
+        const QColor& color = QColorDialog::getColor(palette.color(QPalette::Text),
+                                                     this, QStringLiteral("設定背景顏色"));
+        if(color.isValid())
+        {
+            palette.setColor(QPalette::Text, color);
+            displayTextEdit->setPalette(palette);
+        }
+    }
+    if(btn == errorPushBtn)
+    {
+        QErrorMessage box(this);
+        box.setWindowTitle(QStringLiteral("錯誤訊息盒"));
+        box.showMessage(QStringLiteral("錯誤訊息實例XX"));
+        box.showMessage(QStringLiteral("錯誤訊息實例XX"));
+        box.showMessage(QStringLiteral("錯誤訊息實例XX"));
+        box.exec();
+    }
+    if(btn == filePushBtn){
+        QString FileName = QFileDialog::getOpenFileName(this, tr("開啟檔案"),"."
+                                                        ,tr("任何檔案(*.*)"
+                                                            ";;文字檔(*.txt)"
+                                                            ";;XML檔(*.xml)"));
+        displayTextEdit->setText(FileName);
+
+    }
+    if(btn == progressPushBtn){
+        QProgressDialog progress(tr("正在複製檔案"),
+                                 tr("取消"),
+                                 0,
+                                 10000,
+                                 this);
+        progress.setWindowTitle(tr("進度對話盒"));
+        progress.show();
+        for(int i=0;i<10000;i++)
+        {
+            progress.setValue(i);
+            qApp->processEvents();
+            if(progress.wasCanceled()){
+                break;
+            }
+            qDebug()<< i;
+        }
+        progress.setValue(10000);
+    }
+}
+
